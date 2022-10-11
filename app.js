eaa36d22b8ac99a91faea9ddb4b89de1653c2222
Patricio Garcia Millan <patogarciamillan@gmail.com>
2022-10-10 21:01:43 -0300

    Update

---
+++ app.js
@@ -0,0 +1,41 @@
+async function getInvited() {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+  let id = params.id;
+
+  const result = await fetch(`http://localhost:8000/invited/${id}`)
+  const json = await result.json()
+
+  document.getElementById('nombreAsistente'). value = json.name
+}
+
+getInvited()
+
+
+async function sendForm() {
+  const email = document.getElementById('emailAsistente').value
+  const comment = document.getElementById('comentariosAsistente').value
+
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+  let id = params.id;
+
+  const sendData = await fetch(`http://localhost:8000/invited/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      isConfirmed: true,
+      email,
+      comment
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+
+  const json = await sendData.json()
+  console.log(json)
+}
